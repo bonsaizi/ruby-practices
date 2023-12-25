92e1debfdf31b2f9d9d133c4f24823587a646729
bonsaizi <augustus110042@gmail.com>
2023-12-25 10:32:52 +0900

    FizzBuzz問題の課題提出(修正版)

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,12 @@
 (1..20).each do |x|
-  if (x%3 == 0)
+  if (x%15 == 0)
+    puts "FizzBuzz"
+  elsif (x%3 == 0)
     puts "Fizz"
   elsif (x%5 == 0)
     puts "Buzz"
-  elsif (x%15 == 0)
-    puts "FizzBuzz"
-  else 
+  else
     puts x
   end
 end
-
 
